050eb3acacb2926ad297abd6ff19cae4fea98623
Fred Westrom <fwestrom@gmail.com>
2018-06-29 14:10:41 -0500

    Fixed failure loading dev.js.

--- commands/dev.js
+++ commands/dev.js
@@ -38,7 +38,9 @@ module.exports = function(_, child_process, fs, inject, io, logging, options, os
             .map(inject)
             .spread(dev);
 
-        function dev(build, clean, compose, deploy, pack, broker, firehose, test) {
+        // function dev(build, clean, compose, deploy, pack, broker, firehose, test) {
+        function dev(build) {
+
 
             return _.extend(function() {
                 io.out('Please specify a dev command.');
